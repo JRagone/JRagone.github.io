42b2b5f8fcc26c439e2e60e1eb2b5e19eec80edf
John Ragone <jragone@umich.edu>
2020-05-04 12:55:16 -0400

    added js for linking with navbar and some more images

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".nav-link").click(function() {
+        try {
+            var navbarHeight = $(this).closest("nav").height();
+            var url = $(this).attr("href");
+            var hash = url.substring(url.indexOf("#")+1);
+            var p = $("#" + hash).first();
+            var position = p.position().top;
+            var top = position - navbarHeight;
+            $("html").scrollTop(top);
+        }
+        catch(err) {
+            console.error(err);
+        }
+    })
+});
